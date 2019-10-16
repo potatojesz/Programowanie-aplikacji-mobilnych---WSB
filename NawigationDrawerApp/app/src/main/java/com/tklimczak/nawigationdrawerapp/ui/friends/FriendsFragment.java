2fc3261aa3aa6ab2d5ea4c861d742dcb9df6f88c
potatojesz <tklimczak92@gmail.com>
2019-10-16 18:42:47 +0200

    drawer app

---
+++ NawigationDrawerApp/app/src/main/java/com/tklimczak/nawigationdrawerapp/ui/friends/FriendsFragment.java
@@ -0,0 +1,35 @@
+package com.tklimczak.nawigationdrawerapp.ui.friends;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.tklimczak.nawigationdrawerapp.R;
+
+public class FriendsFragment extends Fragment {
+
+    private FriendsViewModel friendsViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        friendsViewModel =
+                ViewModelProviders.of(this).get(FriendsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_friends, container, false);
+        final TextView textView = root.findViewById(R.id.text_friends);
+        friendsViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
